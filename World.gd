c12ceba6be40c00c51d51c76e8038794d74b4314
SyntaX <michael.leithner@gmail.com>
2021-05-20 19:41:51 +0200

    Increase score if player dies, game over after one player reaches enough points

--- World.gd
+++ World.gd
@@ -1,24 +1,28 @@
 extends Node
 
-export var round_time := 90.0 # total round time in seconds
+export var cycle_time := 90.0 # total round time in seconds
+export var win_score := 5
 
 var left_time # round timer in ms
 # TODO: combine into array
-var player_manager # holds information about active player and ghosts
-var player_manager2
+#var player_manager # holds information about active player and ghosts
 
-# TODO: combine into scene  with access functions
+var player1 # first player, used to connect score to players
+
+# TODO: combine into UI scene with access functions
 var label_timer # display for remaining round time
 var label_score1 # score for player 1
 var label_score2 # score for player 2
 
 
 func _ready():
-	left_time = round_time # round timer in ms
+	left_time = cycle_time
 	
 	for player_manager in $PlayerManagers.get_children():
-		player_manager.connect("active_player_died", self, "_on_active_player_died")
-		player_manager.connect("ghost_player_died", self, "_on_ghost_player_died")
+		if player1 == null:
+			player1 = player_manager.active_player
+		player_manager.connect("active_player_died", self, "_on_active_player_died", [player_manager.active_player])
+		player_manager.connect("ghost_player_died", self, "_on_ghost_player_died", [player_manager.active_player])
 
 	label_timer = $Timer
 	label_score1 = $Score
@@ -28,7 +32,7 @@ func _ready():
 func _process(delta):
 	left_time -= delta
 	
-	# For UI
+	# for UI
 	var seconds = int(left_time)
 	label_timer.set_text("{mm}:{ss}".format({
 						 "mm":"%02d" % (seconds / 60),
@@ -45,22 +49,32 @@ func _process(delta):
 #	- reset player starting point
 # 	- restarting level time
 func restart():
-	print("restarting...")
-	left_time = round_time
-	
+	left_time = cycle_time
+	_updateScore(player1)
 	for player_manager in $PlayerManagers.get_children():
 		player_manager.convert_active_to_ghost()
+
+
+func _updateScore(player):
+	var label_score = label_score1 if (player == player1) else label_score2
+	var val = int(label_score.text) + 1
+	label_score.set_text(str(val))
+	if val == win_score:
+		print("game over")
+		get_tree().quit()
 
 
 # one of the current active players died:
 #	- add score
 # 	- restart round
-func _on_active_player_died():
-	print("active player died")
+func _on_active_player_died(player):
+	print("active player died: " + player.name)
+	_updateScore(player)
 	restart()
 
 
 # a ghost clone of one player died:
 # 	- add score
-func _on_ghost_player_died():
+func _on_ghost_player_died(player):
+	_updateScore(player)
 	print("ghost player died")
