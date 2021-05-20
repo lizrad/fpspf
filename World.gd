bf7ec3a2a6d6f56e0eb930d8490e7036a5de1cd5
SyntaX <michael.leithner@gmail.com>
2021-05-20 17:19:52 +0200

    Add Player score to HUD Format timer mm:ss

--- World.gd
+++ World.gd
@@ -3,14 +3,27 @@ extends Node
 export var round_time := 90.0 # total round time in seconds
 
 var left_time # round timer in ms
+# TODO: combine into array
+var player_manager # holds information about active player and ghosts
+var player_manager2
+
+# TODO: combine into scene  with access functions
 var label_timer # display for remaining round time
-var player_manager # holds information about players
+var label_score1 # score for player 1
+var label_score2 # score for player 2
 
 
 func _ready():
 	left_time = round_time # round timer in ms
-	label_timer = $Timer
 	player_manager = $PlayerManager
+	player_manager.connect("active_player_died", self, "_on_active_player_died")
+	player_manager.connect("ghost_player_died", self, "_on_ghost_player_died")
+	player_manager2 = $PlayerManager2
+	player_manager2.connect("active_player_died", self, "_on_active_player_died")
+	player_manager2.connect("ghost_player_died", self, "_on_ghost_player_died")
+	label_timer = $Timer
+	label_score1 = $Score
+	label_score2 = $Score2
 
 
 func _process(delta):
@@ -18,14 +31,12 @@ func _process(delta):
 	
 	# For UI
 	var seconds = int(left_time)
-	# TODO: string format 00:00
-	label_timer.set_text(String(seconds / 60) + ":" + String(seconds % 60))
+	label_timer.set_text("{mm}:{ss}".format({
+						 "mm":"%02d" % (seconds / 60),
+						 "ss":"%02d" % (seconds % 60)}))
 	
 	if left_time <= 0:
 		restart()
-	
-	# TODO: check player health and end round or handle per signals
-	#player_manager.activePlayer...
 
 
 # used to restart level: 
@@ -37,3 +48,17 @@ func _process(delta):
 func restart():
 	print("restarting...")
 	left_time = round_time
+
+
+# one of the current active players died:
+#	- add score
+# 	- restart round
+func _on_active_player_died():
+	print("active player died")
+	restart()
+
+
+# a ghost clone of one player died:
+# 	- add score
+func _on_ghost_player_died():
+	print("ghost player died")
