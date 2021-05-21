514170f28822535a0b7457f54c47e527895012be
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-05-21 16:32:00 +0200

    Add ammunition tracking

--- Attacking/AttackType.gd
+++ Attacking/AttackType.gd
@@ -1,6 +1,7 @@
 class_name AttackType
 extends Resource
 
+export var ammunition: int
 export var cooldown: float
 export var damage: float
 export (PackedScene) var attack
--- Attacking/Attacker.gd
+++ Attacking/Attacker.gd
@@ -3,6 +3,8 @@ extends Spatial
 export var visualization_time := 0.5
 var _attack_deadline := 0.0
 
+var _ammunition_tracker = {}
+
 func attack(attack_type, owning_player) -> void:
 	if _attack_deadline <= 0:
 		_create_attack(attack_type, owning_player)
@@ -10,8 +12,21 @@ func attack(attack_type, owning_player) -> void:
 func _process(delta):
 	if _attack_deadline > 0:
 		_attack_deadline -= delta
+func _handle_ammunition(attack_type) ->bool:
+	if not _ammunition_tracker.has(attack_type):
+		_ammunition_tracker[attack_type]=attack_type.ammunition
+		
+	if _ammunition_tracker[attack_type]==0:
+		return false
+	elif _ammunition_tracker[attack_type]>0:
+		_ammunition_tracker[attack_type]-=1
+		
+	return true
 
 func _create_attack(attack_type, owning_player) -> void:
+	if !_handle_ammunition(attack_type):
+		return
+	print(str("Ammunition is ",_ammunition_tracker[attack_type], "/",attack_type.ammunition))
 	_attack_deadline = attack_type.cooldown
 	var attack = attack_type.attack.instance()
 	get_tree().get_root().add_child(attack);
