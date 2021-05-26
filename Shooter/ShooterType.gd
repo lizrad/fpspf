26c31ce8cb12ca6debce2c97ca0d90f49b1fd820
Karl <karl.bittner@hexaquo.at>
2021-05-26 11:48:00 +0200

    Delete things for some reason?

--- Shooter/ShooterType.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name ShooterType
-
-export var cooldown: float
-export var damage: float
-enum BulletType{Hitscan}
-export(BulletType) var bullet_type
-export var bullet_speed: float
-export var bullet_range: float
