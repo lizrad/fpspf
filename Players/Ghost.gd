c208038d3d0362a36c9aaaa9beabcb0b36cc51c5
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-05-26 16:16:40 +0200

    Implement replay

--- Players/Ghost.gd
+++ Players/Ghost.gd
@@ -18,27 +18,35 @@ signal died
 
 func _ready():
 	set_rendering_for_character_id(CharacterID.GHOST)
-	reset()
 
+	
 func set_time_scale(time_scale: float) -> void:
 	_time_scale = time_scale
 
 func _physics_process(delta):
-	if is_dead:
-		return
+	_frame_timer += delta*abs(_time_scale);
+	while _frame_timer>=delta :
+		_frame_timer -= delta
+		current_frame += (sign(_time_scale)*1)
 	
-	_frame_timer += delta*_time_scale;
-	if abs(_frame_timer)>=delta :
-		_frame_timer -= sign(_time_scale)*delta
-		current_frame += 1
-	
-	if current_frame < movement_record.size() and current_frame>=0:
-		var frame = movement_record[current_frame]
-		global_transform = frame.transform
-		if frame.attack_type:
-			$Attacker.attack(frame.attack_type, self)
-		
-		
+	if _time_scale>=0:
+		if current_frame < movement_record.size():
+			var frame = movement_record[current_frame]
+			global_transform = frame.transform
+			if frame.attack_type:
+				$Attacker.attack(frame.attack_type, self)
+	else :
+		if current_frame>=0:
+			if current_frame > died_at_frame or current_frame>=movement_record.size():
+				_showDead()
+			else:
+				_showAlive()
+				var frame = movement_record[current_frame]
+				global_transform = frame.transform
+				if frame.attack_type:
+					$Attacker.attack(frame.attack_type, self)
+					
+
 func receive_damage(damage: float):
 	print("Ghost received damage: ", damage)
 	current_health -= damage
@@ -50,9 +58,8 @@ func receive_damage(damage: float):
 		_showDead()
 
 
-func reset() -> void:
-	is_dead = false
-	current_frame = 0.0 if _time_scale>=0 else  movement_record.size()-1
+func reset(start_frame : int) -> void:
+	current_frame = start_frame
 	_frame_timer = 0.0
 	_showAlive()
 	_set_initial_position()
@@ -60,16 +67,17 @@ func reset() -> void:
 
 
 func _set_initial_position() -> void:
-	global_transform = movement_record[0].transform
+	var start_frame = current_frame if current_frame < movement_record.size() else movement_record.size()-1
+	global_transform = movement_record[start_frame].transform
 	
 func _showDead():
 	rotation.z = PI / 2
-	set_physics_process(false)
+	#set_physics_process(false)
 	$CollisionShape.disabled = true
 	$Attacker.visible = false
 
 func _showAlive():
 	rotation.z = 0
-	set_physics_process(true)
+	#set_physics_process(true)
 	$CollisionShape.disabled = false
 	$Attacker.visible = true
