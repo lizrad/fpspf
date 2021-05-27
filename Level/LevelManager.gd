fb04859c6e6b8c67241dc4835d7a7afcb5dbe6e5
incredibleLeitman <michael.leithner@gmail.com>
2021-05-27 14:24:38 +0200

    Rename "PlayerBase" to "SpawnArea" to minimize misunderstanding

--- Level/LevelManager.gd
+++ Level/LevelManager.gd
@@ -4,10 +4,9 @@ var _player_base_1
 var _player_base_2
 
 func open_doors():
-	$Level/PlayerBase1.open_doors()
-	$Level/PlayerBase2.open_doors()
-	#($PlayerBase1 as PlayerBase).open_doors()
+	$Level/SpawnArea1.open_doors()
+	$Level/SpawnArea2.open_doors()
 	
 func close_doors():
-	($Level/PlayerBase1 as PlayerBase).close_doors()
-	($Level/PlayerBase2 as PlayerBase).close_doors()
+	($Level/SpawnArea1 as SpawnArea).close_doors()
+	($Level/SpawnArea2 as SpawnArea).close_doors()
--- Level/SpawnArea.gd
+++ Level/SpawnArea.gd
@@ -1,5 +1,5 @@
 extends Spatial
-class_name PlayerBase
+class_name SpawnArea
 
 func open_doors():
 	for child in self.get_children():
