1a0d19173099b73f20a37b3269cd3e46f4d5c381
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-05-27 17:14:41 +0200

    Add burst fire attacks

--- Attacking/AttackType.gd
+++ Attacking/AttackType.gd
@@ -2,6 +2,8 @@ class_name AttackType
 extends Resource
 
 export var ammunition: int
+export var burst_amount: int
+export var burst_delay: float
 export var attack_range: float
 export var attack_time: float
 export var continuously_damaging: bool
