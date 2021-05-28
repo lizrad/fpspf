9b530866d995ce062913493a3dd7649bd4a0cac0
David Panagiotopulos <panagiotopulos.d@gmail.com>
2021-05-28 15:28:47 +0200

    Update GameOverScreen

--- UI/GameOverScreen.gd
+++ UI/GameOverScreen.gd
@@ -4,10 +4,14 @@ class_name GameOverScreen
 signal restart_pressed
 signal quit_pressed
 
+onready var _winner_txt := get_node("PanelContainer/VBoxContainer/Winner_txt")
+onready var _restart_btn := get_node("PanelContainer/VBoxContainer/HBoxContainer/Restart_btn")
+onready var _quit_btn := get_node("PanelContainer/VBoxContainer/HBoxContainer/Quit_btn")
+
 func _ready():
 	hide()
-	$Restart_btn.connect("pressed", self, "_on_Restart_btn_pressed")
-	$Quit_btn.connect("pressed", self, "_on_Quit_btn_pressed")
+	_restart_btn.connect("pressed", self, "_on_Restart_btn_pressed")
+	_quit_btn.connect("pressed", self, "_on_Quit_btn_pressed")
 
 
 func show():
@@ -19,12 +23,12 @@ func hide():
 
 func set_winner(idx : int):
 	if idx >= 0 && idx <= 1:
-		$Winner_txt.text = "Player " + str(idx + 1) + " won!"
-		$Winner_txt.add_color_override("font_color", Constants.character_colors[idx])
-		$Winner_txt.add_color_override("font_outline_modulate", Constants.character_colors[idx])
+		_winner_txt.text = "Player " + str(idx + 1) + " won!"
+		_winner_txt.add_color_override("font_color", Constants.character_colors[idx])
+		_winner_txt.add_color_override("font_outline_modulate", Constants.character_colors[idx])
 	else:
-		$Winner_txt.text = "Draw!"
-		$Winner_txt.add_color_override("font_color", Color.white)
+		_winner_txt.text = "Draw!"
+		_winner_txt.add_color_override("font_color", Color.white)
 
 
 func _on_Restart_btn_pressed():
